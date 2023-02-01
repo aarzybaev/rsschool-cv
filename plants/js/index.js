022ff13e50557bff054bb8bde5052fbc092befc8
Askhad <aarzybaev@gmail.com>
2023-02-01 16:21:51 +0600

    feat: start impliments action in section service

--- plants/js/index.js
+++ plants/js/index.js
@@ -1,79 +1,130 @@
 window.onload = function() {
-    
-    console.log('Scores: 70');
-    addHamburgerMenuOpenClickHandler();
+   
+    init();
     
 }
 
 
-const addHamburgerMenuOpenClickHandler = () => {
+const init = () => {
+    // burger menu
     document.querySelector('.hamburger').addEventListener('click', (e) => {
-
+        
         //create burger-v
         const hamburgerVLine1 = createElement('span', 'hamburger__v-line');
         const hamburgerVLine2 = createElement('span', 'hamburger__v-line');
         const hamburgerVLine3 = createElement('span', 'hamburger__v-line');
         const hamburgerVLine4 = createElement('span', 'hamburger__v-line');
         const hamburgerVWrapper = createElement('span', 'hamburger__v-wrapper');
+        
         appendChild(hamburgerVWrapper, hamburgerVLine1);
         appendChild(hamburgerVWrapper, hamburgerVLine2);
         appendChild(hamburgerVWrapper, hamburgerVLine3);
         appendChild(hamburgerVWrapper, hamburgerVLine4);
+
         //create burger-v
-        
         const hamburgerLayout = createElement('div', 'hamburger__layout');
+        hamburgerLayout.setAttribute('tabindex', "-1");
         const navigationWrapper = getElementsByClassName('navigation__wrapper')[0];
 
         appendChild(hamburgerLayout, hamburgerVWrapper);
         appendChild(hamburgerLayout, navigationWrapper);
+        
         //Insert hamburger layout to end of body
         appendChild(document.getElementsByTagName('body')[0], hamburgerLayout);
         changeClass('navigation__wrapper', 'hamburger__wrapper');
+        
         //animation
-        addClass('hamburger__layout', 'show-block')
+        addClass('hamburger__layout', 'show-block');
+
+        document.querySelector('.hamburger__layout').focus();
+
+        document.querySelector('.hamburger__layout').addEventListener('blur', () => {
+            
+            hideBlockAnimation('hamburger__layout', 'hide-block');
+
+        });
         
         //Menu close handler
         addHamburgerMenuCloseClickHandler();
         addHamburgerMenuItemClickHandler();
-      
-    })
+    });
+
+
+
+
+// Service card select
+
+let buttons = document.querySelector('.service-header__button-wrapper');
+let clickedButtonsCount = 0;
+
+buttons.addEventListener('click', (e) => {
+
+if(!e.target.attributes['data-button']) return;
+
+if (e.target.classList.contains('service-header__button-active')) {
+    
+    if (clickedButtonsCount == 1) {
+            addRemoveClass('blur', 'remove', `[data-service-card]`);
+    } else {
+        addRemoveClass('blur', 'add', `[data-service-card="${e.target.dataset.button}"]`);
+    }
+
+    e.target.classList.remove('service-header__button-active');
+    clickedButtonsCount--;
+
+} else {
+
+    if (clickedButtonsCount < 2) {
+        e.target.classList.add('service-header__button-active');
+
+        if (clickedButtonsCount == 0) {
+            addRemoveClass('blur', 'add', `[data-service-card]:not([data-service-card="${e.target.dataset.button}"])`); 
+        } else {
+            addRemoveClass('blur', 'remove', `[data-service-card="${e.target.dataset.button}"]`); 
+        }
+
+    clickedButtonsCount++;
+
+    }
 }
+
+
+});
+
+};
+
+function addRemoveClass(className, classOperation, filter) {
+
+    let items = document.querySelectorAll(filter);
+
+    for(let element of items) {
+        element.classList[classOperation](className);
+    }   
+}
+
+
+// Service card select
+
 
 const addHamburgerMenuCloseClickHandler = () => {
     document.querySelector('.hamburger__v-wrapper').addEventListener('click', (e) => {
-                
-        removeClassName('show-block');
-
-        hideBlockAnimation('hamburger__layout', 'hide-block', () => {
-            changeClass('hamburger__wrapper', 'navigation__wrapper',);
-            const navigationWrapper = getElementsByClassName('navigation__wrapper')[0];
-            const navigation = getElementsByClassName('navigation')[0]; 
-            appendChild(navigation, navigationWrapper);
-            removeElement('hamburger__layout');
-        });
+        
+        hideBlockAnimation('hamburger__layout', 'hide-block');
         
     })
-
-}
+    
+};
 
 const addHamburgerMenuItemClickHandler = () => {
     document.querySelector('.hamburger__wrapper').addEventListener('click', (e) => {
         if(e.target.className.includes('navigation__link')) {
 
-                hideBlockAnimation('hamburger__layout', 'hide-block', () => {
-
-                    changeClass('hamburger__wrapper', 'navigation__wrapper',);
-                const navigationWrapper = getElementsByClassName('navigation__wrapper')[0];
-                const navigation = getElementsByClassName('navigation')[0]; 
-                    appendChild(navigation, navigationWrapper);
-                    removeElement('hamburger__layout');
-
-            }); 
+                hideBlockAnimation('hamburger__layout', 'hide-block'); 
         }
         
     });
-}
-
+    
+};
 
 
 const addClass = (className, addedClassName) => {
@@ -81,41 +132,57 @@ const addClass = (className, addedClassName) => {
     HTML_Collection.forEach(element => {
         element.classList.add(addedClassName);
     });
-}
+};
 
 const changeClass = (currClassName, repClassName) => {
     let HTML_Collection = getElementsByClassName(currClassName);
         HTML_Collection.forEach(element => {
         element.className = repClassName;
     });
-}
+};
 
 const createElement = (tagName, className) => {
 const newElement = document.createElement(tagName);
       newElement.classList.add(className);
       return newElement;
-}
+};
 
 const removeElement = (className) => {
     const element = getElementsByClassName(className)[0];
     if(element) {
        element.remove();
     } 
-}
+};
 
 const removeClassName = (className) => {
     let element = getElementsByClassName(className)[0];
-    element.classList.remove(className);
-
-}
+    if(element) {
+        element.classList.remove(className);
+    }
+};
 
 const appendChild = (parentTag, childTag) => parentTag.appendChild(childTag);
 
 const getElementsByClassName = (className) => [...document.getElementsByClassName(className)];
 
 const hideBlockAnimation = (className, addedClassName, callback) => {
-    addClass(className, addedClassName);
-    setTimeout(callback, 1000);
     
-}
+    //removeElement('hamburger__v-wrapper');
+    addClass(className, addedClassName);
+
+    let cb = () => {
+        
+        removeClassName('show-block');
+        changeClass('hamburger__wrapper', 'navigation__wrapper',);
+
+        const navigationWrapper = getElementsByClassName('navigation__wrapper')[0];
+        const navigation = getElementsByClassName('navigation')[0]; 
+
+        appendChild(navigation, navigationWrapper);
+        removeElement('hamburger__layout');
+
+    }; 
+    setTimeout(cb, 1000);
+    
+};
 
